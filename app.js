616b7fb2b6c1ffa93bb0e8f5ef98e301268d1bdc
Aaron916 <roo18002@byui.edu>
2022-02-09 11:12:17 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const res = require('express/lib/response');
+const app = express();
+var path = require('path')
+
+app.set('views', './views');
+app.set('view engine', 'jade')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res, next) => {res.render('index')});
+
+
+app.listen(3000);
