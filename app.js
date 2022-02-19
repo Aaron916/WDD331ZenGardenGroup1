20d89b69354faa534647fe5533c36b6cda757c61
exagent957 <exagent957@gmail.com>
2022-02-19 17:36:44 -0600

    marc changed app.js  - app.use

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-const express = require('express');
-const res = require('express/lib/response');
+const express = require("express");
+const res = require("express/lib/response");
 const app = express();
-var path = require('path')
+var path = require("path");
 
-app.set('views', './views');
-app.set('view engine', 'jade')
+app.set("views", "./views");
+app.set("view engine", "jade");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get('/', (req, res, next) => {res.render('index')});
-
+app.use("/", (req, res, next) => {
+  res.render("index");
+});
 
 app.listen(3000);
